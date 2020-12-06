66a46da5f08fabe38deee2d519dd15113298f93a
UT07 <254utkarsh@gmail.com>
2020-12-06 16:06:55 -0600

    Add files via upload

---
+++ 1205_Hello_World.c
@@ -0,0 +1,17 @@
+//For use in CSE 2100 Lab at The University of Texas at Arlington
+//Written by Utkarsh Singh
+
+//Include necessary dependecies
+#include <stdio.h>
+
+//program entry poing
+//argc - number of command line arguments
+//argv - array of strings containing command line arguments
+int main(int argc, char **argv)
+{
+	//output to terminal
+	printf("Hello 1205!\n");
+
+	//end program
+	return 0;
+}
